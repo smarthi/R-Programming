3d25b7731a35f5fe6107e2e550f25d03f87256eb
smarthi <smarthi@apache.org>
2015-12-17 23:57:43 -0500

    committed best

--- week3/best.R
+++ week3/best.R
@@ -1,6 +1,7 @@
 best <- function(state, outcome) {
   ## Read outcome data
   hospital_outcomes <- read.csv("outcome-of-care-measures.csv")
+  
   ## check that state and outcome are valid
   states <- unique(hospital_outcomes[,7])
   outcomes <- c("heart attack", "heart failure", "pneumonia")
@@ -33,22 +34,19 @@ best <- function(state, outcome) {
   
   ## Get rid of NA's from the data
   desired_column <- as.numeric(state_hospital_outcomes[,outcome_column])
-  good <- !is.na(desired_column)
-  state_hospital_outcomes <- state_hospital_outcomes[good,]
-  
   ## Return the hospital name in the state with the lowest
   ## 30-day death rate
   
   ## Get the rows with min outcome values
-  desired_column <- as.numeric(state_hospital_outcomes[,outcome_column])
-  desired_rows <- which(desired_column == min(desired_column))
+  desired_rows <- which.min(desired_column)
+  print(desired_rows)
+  print(state_hospital_outcomes[desired_rows, 1:3])
   candidate_hospitals <- state_hospital_outcomes[desired_rows,2]
-
+  
   ## Sort by the hospital names and 
   ## return the first one from the sorted list
   if (length(candidate_hospitals) > 1) {
-    candidate_hospitals_sorted <- sort(candidate_hospitals)
-    print(candidate_hospitals_sorted)
+    candidate_hospitals_sorted <- sort(unique(candidate_hospitals))
     as.vector(candidate_hospitals_sorted[1])
   } else {
     as.vector(candidate_hospitals[1])
