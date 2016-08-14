dcbdbbbf9c90257af0b43f7f29e89a85d144ef44
smarthi <smarthi@apache.org>
2016-08-14 15:04:25 -0400

    minor tweaks

--- week2/complete.R
+++ week2/complete.R
@@ -1,14 +1,21 @@
 complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
   
-  results <- matrix(ncol=2, nrow = 0)
-  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  filenames = list.files(directory, full.names = TRUE)
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  airquality <- read.table(text="", header=TRUE, col.names = c('id', 'nobs'))
   for (i in id) {
-    data <- getmonitor(i, directory)
-    temp <- data[!is.na(data[,"sulfate"]),]
-    temp <- temp[!is.na(temp[,"nitrate"]),]
-    results <- rbind(results, c(i, nrow(temp)))  
+    df <- rbind(airquality, c(i, sum(complete.cases(read.csv(filenames[i])))))
   }
-  
-  dimnames(results) = list(1:nrow(results), c("id","nobs"))
-  data.frame(results)
+  colnames(airquality) <- c("id", "nobs")
+  df
 }
